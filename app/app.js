f1dec57bf671efbc684430793216657bebadf1b4
Lukas Frantzke <lukas@simplista.com>
2023-09-21 11:00:11 -0400

    Introduce userIds and private rooms

--- app/app.js
+++ app/app.js
@@ -1,24 +1,45 @@
-const socket = io('ws://localhost:8080');
+let socket;
 
-socket.on('message', (text) => {
-  const el = document.createElement('li');
-  el.innerHTML = text;
-  document.querySelector('ul').appendChild(el);
-});
+const connectToServer = () => {
+  const userId = document.getElementById('userIdInput').value;
+  socket = io(`ws://localhost:8080?userId=${userId}`);
 
-const input = document.querySelector('input');
+  document.getElementById('messageInput').style.display = "inline-block	";
+  document.getElementById('messageButton').style.display = "inline-block	";
+  document.getElementById('userIdInput').style.display = "none";
+  document.getElementById('connectButton').style.display = "none";
+
+  socket.on('connect', () => {
+    console.log(`You've connected with id: ${socket.id}`);
+  });
+
+  socket.on('message', (text) => {
+    const el = document.createElement('li');
+    el.innerHTML = text;
+    document.querySelector('ul').appendChild(el);
+  });
+};
+
+const userIdInput = document.getElementById('userIdInput');
+const connectButton = document.getElementById('connectButton');
+connectButton.onclick = connectToServer;
 
 const emitText = () => {
-    const text = input.value;
-    socket.emit('message', text);
-    input.value = '';
-}
+  const text = messageInput.value;
+  socket.emit('message', text);
+  messageInput.value = '';
+};
 
-document.querySelector('button').onclick = emitText;
-
-input.addEventListener('keypress', function (event) {
+const messageInput = document.getElementById('messageInput');
+messageInput.addEventListener('keypress', function (event) {
   if (event.key === 'Enter') {
     event.preventDefault();
     emitText();
   }
 });
+messageInput.style.display = 'none';
+
+const messageButton = document.getElementById('messageButton');
+messageButton.onclick = emitText;
+messageButton.style.display = 'none';
+
--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
@@ -28,17 +27,23 @@ const io = require('socket.io')(http, {
 
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
-  connections.push(socket.id)
+  connections.push(socket.id);
+  const userId = socket.handshake.query.userId || "Unknown";
+  io.emit("message", `User ${userId} connected`);
 
   socket.on('message', (message) => {
     console.log(message);
-    io.emit('message', `${socket.id.substr(0, 4)}: ${message}`);
+    io.emit('message', `${userId}: ${message}`);
+    // socket.broadcast.emit('message', `Broadcast Message`); //Send to everyone BUT this socket
   });
 
+  socket.join(userId);
+  io.to(userId).emit('message', `Private: Welcome to the chat ${userId}!`);
 
   socket.on('disconnect', (reason) => {
-    console.log('user disconeected');
+    console.log('user disconnected');
     console.log('reason:', reason);
+    io.emit('message', `User ${userId} disconnected`);
   });
 });
 
